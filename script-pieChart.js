ca338b5f572452833547af5d532bd15aa7dfb12d
georgiagelasco <124117294+georgiagelasco@users.noreply.github.com>
2024-11-17 19:07:16 -0500

    Update script-pieChart.js

--- script-pieChart.js
+++ script-pieChart.js
@@ -1,102 +1,60 @@
-
-//import data!!!
 d3.csv("covid.csv").then(
     function(data){
+
         var dimensions = {
-            margin : {top: 20, right: 20, bottom: 20, left: 20},
-            //ccellSize = XXX;
-            width : 500, //data[0].length * cellSize;
-            height : 450, //data.length * cellSize;
-            cellSize : 50
+            width: 500,
+            height: 500,
+            radius : Math.min(width, height) / 2
         }
 
-
-        //groups
-        var ageGroups = Array.from(new Set(data.map(d => d.age_group)))
-        var raceEthnicities = Array.from(new Set(data.map(d => `${d.race} (${d.ethnicity})`)))
-
-        const svg = d3.select("#heatmap")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
-
-        //count occrrences
-        var counts = d3.rollup(
+        var sexCounts = d3.rollup(
             data,
             v => v.length,
-            d => d.age_group,
-            d => `${d.race} (${d.ethnicity})`
+            d => d.sex
         )
+        
+        var sexData = Array.from(sexCounts, ([key, value]) => ({ sex: key, count: value }))
 
+       var svg = d3.select("#piechart")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", `translate(${width / 2}, ${height / 2})`)
 
-        //get heatmao data
-        var heatmapData = []
-        ageGroups.forEach((ageGroup, rowIndex) =>{
-            raceEthnicities.forEach((raceEthnicity, colIndex) =>{
-                var count = counts.get(ageGroup)?.get(raceEthnicity) || 0
-                heatmapData.push({ ageGroup, raceEthnicity, rowIndex, colIndex, count })
-            })
-        })
+        //color
+        var color = d3.scaleOrdinal()
+            .domain(sexData.map(d => d.sex))
+            .range(d3.schemeCategory10)
 
+        //creating pie
+        var pie = d3.pie()
+            .value(d => d.count)
 
-        //set color definition
+        var arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius)
 
-
-        //color scale
-        //const colorScale = d3.scaleSequential(d3.interpolateBlues)
-            //.domain()//need to know range
-            var maxCount = d3.max(heatmapData, d => d.count)
-            var colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0, maxCount])
-
-
-        //create the grid cells
-        svg.selectAll("rect")
-            //2D data.flat
-            .data(heatmapData)
+        //draw chart
+        svg.selectAll("path")
+            .data(pie(sexData))
             .enter()
-            .append("rect")
-            //x and y dimentions
-            .attr("x", d => d.colIndex * cellSize)
-            .attr("y", d => d.rowIndex * cellSize)
-            .attr("width", cellSize)
-            .attr("height", cellSize)
-            .style("fill", d => colorScale(d.count))
-            .style("stroke", "#ccc");
-
-
-
+            .append("path")
+            .attr("d", arc)
+            .attr("fill", d => color(d.data.sex))
+            .attr("stroke", "white")
+            .attr("stroke-width", 2)
 
         //labels
-        var xScale = d3.scaleBand().domain(raceEthnicities).range([0, raceEthnicities.length * cellSize])
-        var yScale = d3.scaleBand().domain(ageGroups).range([0, ageGroups.length * cellSize])
-
-        svg.append("g")
-            .selectAll("text")
-            .data(raceEthnicities)
+        svg.selectAll("text")
+            .data(pie(sexData))
             .enter()
             .append("text")
-            .attr("x", (_, i) => i * cellSize + cellSize / 2)
-            .attr("y", -5)
+            .attr("transform", d => `translate(${arc.centroid(d)})`)
             .attr("text-anchor", "middle")
-            .attr("font-size", "10px")
-            .text(d => d);
+            .style("font-size", "12px")
+            .style("fill", "white")
+            .text(d => `${d.data.sex}: ${d.data.count}`);
 
-        svg.append("g")
-            .selectAll("text")
-            .data(ageGroups)
-            .enter()
-            .append("text")
-            .attr("x", -5)
-            .attr("y", (_, i) => i * cellSize + cellSize / 2)
-            .attr("text-anchor", "end")
-            .attr("font-size", "10px")
-            .text(d => d);
-
-        
-            
 
     }
-
 )
