d8ebc0d9857d9ae2dfb1308310afab70fe12349a
georgiagelasco <124117294+georgiagelasco@users.noreply.github.com>
2024-12-10 16:00:32 -0500

    everything is now on one pg script.js

--- script.js
+++ script.js
@@ -176,99 +176,87 @@ function updateBarChart(attribute) {
 
 // Update Heatmap
 function updateHeatmap() {
-    d3.csv("covid.csv").then(function(data) {
-        var dimensions = {
-            margin: {
-                top: 50,
-                bottom: 100,
-                right: 50,
-                left: 100
-            },
-            cellSize: 50, // Adjust for better visualization
-            width: 0,     // Will be calculated dynamically
-            height: 0
-        };
+   // set the dimensions and margins of the graph
+var margin = {top: 30, right: 30, bottom: 30, left: 30},
+  width = 450 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
 
-        // Extract unique age groups and race/ethnicities
-        var ageGroups = Array.from(new Set(data.map(d => d.age_group)));
-        var raceEthnicities = Array.from(new Set(data.map(d => `${d.race} (${d.ethnicity})`)));
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-        // Update width and height based on the data and cell size
-        dimensions.width = raceEthnicities.length * dimensions.cellSize;
-        dimensions.height = ageGroups.length * dimensions.cellSize;
+// Labels of row and columns
+var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"]
 
-        console.log(dimensions.height)
-        console.log(dimensions.width)
+// Build X scales and axis:
+var x = d3.scaleBand()
+  .range([ 0, width ])
+  .domain(myGroups)
+  .padding(0.01);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x))
 
-        // Create SVG container
-        const svg = d3.select("#heatmap")
-            .append("svg")
-            .attr("width", dimensions.width + dimensions.margin.left + dimensions.margin.right)
-            .attr("height", dimensions.height + dimensions.margin.top + dimensions.margin.bottom)
-            .append("g")
-            .attr("transform", `translate(${dimensions.margin.left},${dimensions.margin.top})`);
+// Build X scales and axis:
+var y = d3.scaleBand()
+  .range([ height, 0 ])
+  .domain(myVars)
+  .padding(0.01);
+svg.append("g")
+  .call(d3.axisLeft(y));
 
-        // Count occurrences of each age group and race/ethnicity combination
-        var counts = d3.rollup(
-            data,
-            v => v.length,
-            d => d.age_group,
-            d => `${d.race} (${d.ethnicity})`
-        );
+// Build color scale
+var myColor = d3.scaleLinear()
+  .range(["white", "#69b3a2"])
+  .domain([1,100])
 
-        // Generate heatmap data
-        var heatmapData = [];
-        ageGroups.forEach((ageGroup, rowIndex) => {
-            raceEthnicities.forEach((raceEthnicity, colIndex) => {
-                var count = counts.get(ageGroup)?.get(raceEthnicity) || 0;
-                heatmapData.push({ ageGroup, raceEthnicity, rowIndex, colIndex, count });
-            });
-        });
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
 
-        // Define color scale
-        var maxCount = d3.max(heatmapData, d => d.count);
-        var colorScale = d3.scaleSequential(d3.interpolateBlues)
-            .domain([0, maxCount]);
+  // create a tooltip
+  var tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
 
-        // Create grid cells
-        svg.selectAll("rect")
-            .data(heatmapData)
-            .enter()
-            .append("rect")
-            .attr("x", d => d.colIndex * dimensions.cellSize)
-            .attr("y", d => d.rowIndex * dimensions.cellSize)
-            .attr("width", dimensions.cellSize)
-            .attr("height", dimensions.cellSize)
-            .style("fill", d => colorScale(d.count))
-            .style("stroke", "#ccc");
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    tooltip.style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    tooltip
+      .html("The exact value of<br>this cell is: " + d.value)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    tooltip.style("opacity", 0)
+  }
 
-        // Add labels for columns (race/ethnicities)
-        svg.selectAll(".colLabel")
-            .data(raceEthnicities)
-            .enter()
-            .append("text")
-            .attr("class", "colLabel")
-            .attr("x", (_, i) => i * dimensions.cellSize + dimensions.cellSize / 2)
-            .attr("y", -10)
-            .attr("text-anchor", "middle")
-            .attr("font-size", "12px")
-            .attr("fill", "black")
-            .text(d => d)
-            .on("click", function(event, d){
-                d3.select(this).style("fill", "gray")
-            });
+  // add the squares
+  svg.selectAll()
+    .data(data, function(d) {return d.group+':'+d.variable;})
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.group) })
+      .attr("y", function(d) { return y(d.variable) })
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.value)} )
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
+})
 
-        // Add labels for rows (age groups)
-        svg.selectAll(".rowLabel")
-            .data(ageGroups)
-            .enter()
-            .append("text")
-            .attr("class", "rowLabel")
-            .attr("x", -10)
-            .attr("y", (_, i) => i * dimensions.cellSize + dimensions.cellSize / 2)
-            .attr("text-anchor", "end")
-            .attr("font-size", "12px")
-            .attr("fill", "black")
-            .text(d => d);
-    });
 }
